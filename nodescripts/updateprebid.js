6445907d6cf0fb65775d30d02a7fad4e77eca2b8
tegner <jtegner@gmail.com>
2020-08-28 11:16:32 +0200

    more build options

--- nodescripts/updateprebid.js
+++ nodescripts/updateprebid.js
@@ -1,21 +1,43 @@
+const fs = require('fs');
 const exec = require('child_process').exec;
 
-const updatePrebid = (version, norubicon) => {
+let modules = [
+  'adformBidAdapter',
+  'appnexusBidAdapter',
+  'criteoBidAdapter',
+  'pubmaticBidAdapter',
+  'consentManagement',
+  'gdprEnforcement',
+  // 'pubCommonIdSystem',
+];
+
+const updatePrebid = (version, norubicon, addModules) => {
   if (!version) {
     console.error('Version missing, nothing to download');
     process.exit(1);
   }
 
+  if (!norubicon) {
+    modules.push('rubiconBidAdapter');
+  }
+
+  if (addModules) {
+    modules = [...modules, ...addModules];
+  }
+
+  const modulesString = `modules%5B%5D=${modules.join('&modules%5B%5D=')}`;
+
+  const dataRaw = `${modulesString}&version=${version}`;
+
   const outputFile = norubicon
     ? './prebid/prebid-norubicon.js'
     : './prebid/prebid.js';
 
-  const newVersion = `curl 'https://js-download.prebid.org/download'   -H 'authority: js-download.prebid.org'   -H 'pragma: no-cache'   -H 'cache-control: no-cache'   -H 'accept: */*'   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'   -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8'   -H 'origin: http://prebid.org'   -H 'sec-fetch-site: cross-site'   -H 'sec-fetch-mode: cors'   -H 'sec-fetch-dest: empty'   -H 'referer: http://prebid.org/download.html'   -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8,da;q=0.7'   --data-raw 'modules%5B%5D=adformBidAdapter&modules%5B%5D=appnexusBidAdapter&modules%5B%5D=criteoBidAdapter&modules%5B%5D=pubmaticBidAdapter&modules%5B%5D=rubiconBidAdapter&modules%5B%5D=consentManagement&modules%5B%5D=gdprEnforcement&version=${version}'   --compressed -o ${outputFile}`;
-  const newVersionNoRubicon = `curl 'https://js-download.prebid.org/download'   -H 'authority: js-download.prebid.org'   -H 'pragma: no-cache'   -H 'cache-control: no-cache'   -H 'accept: */*'   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'   -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8'   -H 'origin: http://prebid.org'   -H 'sec-fetch-site: cross-site'   -H 'sec-fetch-mode: cors'   -H 'sec-fetch-dest: empty'   -H 'referer: http://prebid.org/download.html'   -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8,da;q=0.7'   --data-raw 'modules%5B%5D=adformBidAdapter&modules%5B%5D=appnexusBidAdapter&modules%5B%5D=criteoBidAdapter&modules%5B%5D=pubmaticBidAdapter&modules%5B%5D=consentManagement&modules%5B%5D=gdprEnforcement&version=${version}'   --compressed -o ${outputFile}`;
+  fs.copyFileSync(outputFile, outputFile.replace('.js', '-prev.js'));
 
-  const execFunc = norubicon ? newVersionNoRubicon : newVersion;
+  const newVersion = `curl 'https://js-download.prebid.org/download'   -H 'authority: js-download.prebid.org'   -H 'pragma: no-cache'   -H 'cache-control: no-cache'   -H 'accept: */*'   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'   -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8'   -H 'origin: http://prebid.org'   -H 'sec-fetch-site: cross-site'   -H 'sec-fetch-mode: cors'   -H 'sec-fetch-dest: empty'   -H 'referer: http://prebid.org/download.html'   -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8,da;q=0.7'   --data-raw '${dataRaw}'   --compressed -o ${outputFile}`;
 
-  exec(execFunc, (err) => {
+  exec(newVersion, (err) => {
     if (err) {
       // node couldn't execute the command
       console.warn(
