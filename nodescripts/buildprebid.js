0d26dba3f67c26fbd57f17a8a4dee834ff05c2b1
tegner <jtegner@gmail.com>
2020-08-28 10:07:24 +0200

    removed console

--- nodescripts/buildprebid.js
+++ nodescripts/buildprebid.js
@@ -2,7 +2,7 @@ const uglifier = require('./packprebid');
 const updatePrebid = require('./updateprebid');
 
 const runtimeArguments = process.argv.slice(2);
-console.log(runtimeArguments);
+
 const envArg = runtimeArguments.find((val) => val.indexOf('env=') !== -1);
 const versionArg = runtimeArguments.find(
   (val) => val.indexOf('version=') !== -1
@@ -17,8 +17,6 @@ const env = envArg.split('=')[1];
 const version = versionArg ? versionArg.split('=')[1] : null;
 
 const noRubicon = !!noRubiconArg;
-
-console.log('envArg', envArg, 'env env env env ', env, version, noRubicon);
 
 if (version) {
   updatePrebid(version, noRubicon);
