311c4a1212ba247b2aa3d9b9f818fb1de867dd90
tegner <jesper.h.tegner@jppol.dk>
2022-01-13 10:49:29 +0100

    EBCD-14613 . no more pubid suffix

--- nodescripts/updateprebid.js
+++ nodescripts/updateprebid.js
@@ -2,13 +2,16 @@ const fs = require('fs');
 const exec = require('child_process').exec;
 
 let modules = [
-  'adformBidAdapter',
+  'adfBidAdapter',
   'appnexusBidAdapter',
   'criteoBidAdapter',
   'pubmaticBidAdapter',
   'consentManagement',
+  'enrichmentFpdModule',
   'gdprEnforcement',
+  'gptPreAuction',
   'sharedIdSystem',
+  'pubProvidedIdSystem',
 ];
 
 const updatePrebid = (updateOptions) => {
@@ -36,7 +39,7 @@ const updatePrebid = (updateOptions) => {
   if (fs.existsSync(outputFile))
     fs.copyFileSync(outputFile, outputFile.replace('.js', '-prev.js'));
 
-  const newVersion = `curl 'https://js-download.prebid.org/download'   -H 'authority: js-download.prebid.org'   -H 'pragma: no-cache'   -H 'cache-control: no-cache'   -H 'accept: */*'   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'   -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8'   -H 'origin: http://prebid.org'   -H 'sec-fetch-site: cross-site'   -H 'sec-fetch-mode: cors'   -H 'sec-fetch-dest: empty'   -H 'referer: http://prebid.org/download.html'   -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8,da;q=0.7'   --data-raw '${dataRaw}'   --compressed -o ${outputFile}`;
+  const newVersion = `curl 'https://js-download.prebid.org/download' -H 'authority: js-download.prebid.org'   -H 'pragma: no-cache'   -H 'cache-control: no-cache'   -H 'accept: */*'   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'   -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8'   -H 'origin: http://prebid.org'   -H 'sec-fetch-site: cross-site'   -H 'sec-fetch-mode: cors'   -H 'sec-fetch-dest: empty'   -H 'referer: http://prebid.org/download.html'   -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8,da;q=0.7'   --data-raw '${dataRaw}'   --compressed -o ${outputFile}`;
   return new Promise((resolve, reject) => {
     exec(newVersion, (err, stdout, stderr) => {
       if (err) {
