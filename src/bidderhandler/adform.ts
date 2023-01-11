77579eff7d6580a46be8b810a66191aa3c006198
Thomas Carlsen <thomascarlsen86@gmail.com>
2023-01-11 09:37:58 +0100

    firstpartyid->jppol.dk
    https://jira-jppol.atlassian.net/browse/EBCD-15494

--- src/bidderhandler/adform.ts
+++ src/bidderhandler/adform.ts
@@ -34,7 +34,7 @@ export const adformBidder = (
     if (eId) {
       adformObject.params.eids = encodeEIDs([
         {
-          source: 'firstpartyid',
+          source: 'jppol.dk’',
           uids: [
             {
               atype: 1,
